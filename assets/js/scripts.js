4872daed2d5fb901fa81e732efbd422e0117067a
Machiko Lacey <machiko_kimura_lacey@yahoo.co.uk>
2020-04-28 14:05:22 +0100

    Changed font settings

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,9 @@
 $(document).ready(
     function(){
- 
+       // This code was taken from StackOverflow
+       //https://stackoverflow.com/questions/24514717/bootstrap-navbar-active-state-not-working
         let pageName = location.pathname.substr(location.pathname.lastIndexOf('/') + 1);
-
         $.each($('.navbar-nav').find('a'), function() {
-
             $(this).toggleClass('active', 
                pageName.indexOf($(this).attr('href')) > -1);
         }); 
